87dd5167afe396531fb2de2826f964a522871262
Jason Forrester <jason4ester1@gmail.com>
2024-07-22 14:12:36 -0500

    GLAB submmition

---
+++ src/main/java/inhereitanceDemo/Hospital.java
@@ -0,0 +1,11 @@
+package inhereitanceDemo;
+
+public class Hospital {
+
+    public static void main(String args[]) {
+        Surgeon s = new Surgeon();
+        s.Doctor_Details();
+        s.Surgeon_Details();
+    }
+
+}
